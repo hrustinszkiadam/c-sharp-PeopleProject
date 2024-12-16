eee9e7f4faf8c80d7caff035b441e6e7449a8bae
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-12-16 18:18:54 +0100

    Person refactor + PersonTest

--- PeopleProject/Person.cs
+++ PeopleProject/Person.cs
@@ -8,32 +8,25 @@ namespace PeopleProject
 {
 	public class Person
 	{
-		private int id;
-		private string name;
-		private int age;
-		private bool isStudent;
-		private int score;
-
-		public int Id { get => id; }
-		public string Name { get => name; }
-		public int Age { get => age; }
-		public bool IsStudent { get => isStudent; }
-		public int Score { get => score; }
-
+		public int Id { get; init; }
+		public string Name { get; init; }
+		public int Age { get; init; }
+		public bool IsStudent { get; init; }
+		public int Score { get; init; }
 
 		public Person(int id, string name, int age, bool isStudent, int score)
 		{
-			if(id <= 0) throw new ArgumentException("Az id számozása 1-től kezdődik", nameof(id));
-            if(score < 0 || score > 100) throw new ArgumentException("A pontszám 0 és 100 közötti szám lehet", nameof(score));
+			if (id <= 0) throw new ArgumentException("Az id számozása 1-től kezdődik", nameof(id));
+            if (score < 0 || score > 100) throw new ArgumentException("A pontszám 0 és 100 közötti szám lehet", nameof(score));
 			if (age < 0) throw new ArgumentException("Az életkor nem lehet negatív szám", nameof(age));
 			if (string.IsNullOrEmpty(name)) throw new ArgumentException("A név nem lehet üres", nameof(name));
 			if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("A név nem lehet csak szóköz", nameof(name));
 
-			this.id = id;
-			this.name = name;
-			this.age = age;
-			this.isStudent = isStudent;
-			this.score = score;
+			Id = id;
+			Name = name;
+			Age = age;
+			IsStudent = isStudent;
+			Score = score;
 		}
 	}
 }
---
+++ TestPeopleProject/PersonTest.cs
@@ -0,0 +1,64 @@
+﻿using PeopleProject;
+
+namespace TestPeopleProject
+{
+	[TestFixture]
+	[TestOf(typeof(Person))]
+	[Category("Person Class Tesztek")]
+	public class PersonTests
+	{
+		[Test]
+		[Category("Konstruktor Inicializálás")]
+		[TestCase(1, "John Doe", 0, true, 0)]
+		[TestCase(2, "Jane Doe", 50, false, 100)]
+		[TestCase(3, "Jack Doe", 100, true, 50)]
+		[TestCase(4, "Jill Doe", 25, false, 75)]
+		public void Letrehozas_HelyesenMukodik(int id, string name, int age, bool isStudent, int score)
+		{
+			Person person = new Person(id, name, age, isStudent, score);
+
+			Assert.That(person.Id, Is.EqualTo(id));
+			Assert.That(person.Name, Is.EqualTo(name));
+			Assert.That(person.Age, Is.EqualTo(age));
+			Assert.That(person.IsStudent, Is.EqualTo(isStudent));
+			Assert.That(person.Score, Is.EqualTo(score));
+		}
+
+		[Test]
+		[Category("Id Invalid input")]
+		[TestCase(-1)]
+		[TestCase(0)]
+		public void InvalidId_ArgumentExceptiontDob(int id)
+		{
+			Assert.Throws<ArgumentException>(() => new Person(id, "John Doe", 0, true, 0));
+		}
+
+		[Test]
+		[Category("Score Invalid input")]
+		[TestCase(-1)]
+		[TestCase(101)]
+		public void InvalidScore_ArgumentExceptiontDob(int score)
+		{
+			Assert.Throws<ArgumentException>(() => new Person(1, "John Doe", 0, true, score));
+		}
+
+		[Test]
+		[Category("Age Invalid input")]
+		[TestCase(-1)]
+		public void InvalidAge_ArgumentExceptiontDob(int age)
+		{
+			Assert.Throws<ArgumentException>(() => new Person(1, "John Doe", age, true, 0));
+		}
+
+		[Test]
+		[Category("Name Invalid input")]
+		[TestCase(null)]
+		[TestCase("")]
+		[TestCase(" ")]
+		public void InvalidName_ArgumentExceptiontDob(string name)
+		{
+			Assert.Throws<ArgumentException>(() => new Person(1, name, 0, true, 0));
+		}
+	}
+
+}
